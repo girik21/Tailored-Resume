fe8823b7d4079af272452c4d8c12eeeab180d08f
dawit-kiros <dwt.krs@gmail.com>
2024-03-23 21:40:25 -0700

    ngx-admin frontend

---
+++ ngx-frontend/src/environments/environment.ts
@@ -0,0 +1,21 @@
+/**
+ * @license
+ * Copyright Akveo. All Rights Reserved.
+ * Licensed under the MIT License. See License.txt in the project root for license information.
+ */
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCPFCMSkeRtJJhiu_yWLIggkRpFL_OM88M",
+    authDomain: "test-app-133d0.firebaseapp.com",
+    projectId: "test-app-133d0",
+    storageBucket: "test-app-133d0.appspot.com",
+    messagingSenderId: "166874967658",
+    appId: "1:166874967658:web:53ef3ec2bb8456d9e2a413"
+  }
+};
