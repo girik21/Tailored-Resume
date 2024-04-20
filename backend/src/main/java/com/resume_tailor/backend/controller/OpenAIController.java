8c4001d788b3b96abe4a2c97cc1319410b8ef613
dawit-kiros <dwt.krs@gmail.com>
2024-04-19 21:53:51 -0700

    Initial Commit

--- backend/src/main/java/com/resume_tailor/backend/controller/OpenAIController.java
+++ backend/src/main/java/com/resume_tailor/backend/controller/OpenAIController.java
@@ -87,6 +87,51 @@ public class OpenAIController {
 
     }
 
+    /**
+     * Creates a chat request and sends it to the OpenAI API
+     * Returns the first message from the API response
+     *
+     * @param requestBody containing the jobDescription and sample resume to send to the API
+     * @return first message from the API response
+     */
+    @PostMapping("/chat/cover-letter/{email}")
+    public ResponseEntity<?> chatCoverLetter(@PathVariable String email, @RequestBody Map<String, String> requestBody) {
+        try {
+            var user = userService.getUserByEmail(email);
+
+            String jobDesc = requestBody.get("jobDesc");
+            String sampleResume = openAIService.generateEscapedCoverLetter(user);
+            String prompt = openAIService.createOpenAICoverLetterPrompt(jobDesc, sampleResume);
+
+            ChatRequest request = new ChatRequest(model, prompt);
+
+            ChatResponse response = restTemplate.postForObject(
+                    apiUrl,
+                    request,
+                    ChatResponse.class);
+
+            if (response == null || response.getChoices() == null || response.getChoices().isEmpty()) {
+                return ResponseEntity.notFound().build();
+            }
+            String jsonString = response.getChoices().get(0).getMessage().getContent();
+
+            ObjectMapper objectMapper = new ObjectMapper();
+
+            String validatedString = openAIService.validateAndFixJson(jsonString);
+
+            // Convert the JSON-formatted string to a JsonNode (JSON object)
+            JsonNode jsonNode = objectMapper.readTree(validatedString);
+
+            // Return the JsonNode
+            return ResponseEntity.ok().body(new ResponseWrapper<>(true, "Cover letter generated successfully.", jsonNode));
+
+        } catch (JsonProcessingException e) {
+            e.printStackTrace(); // Log the exception for debugging
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ResponseWrapper<>(false, e.getMessage(), null));
+        }
+
+    }
+
     @PostMapping("/chat/experiences")
     public ResponseEntity<?> experienceGenerator(@RequestBody Map<String, String> requestBody) {
         try {
